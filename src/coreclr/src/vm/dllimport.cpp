1a3bbc21c0af04bfc4e17db1663900c0af8c8008
vsadov <vsadov@microsoft.com>
2020-07-15 14:02:52 -0700

    a few nits

--- src/coreclr/src/vm/dllimport.cpp
+++ src/coreclr/src/vm/dllimport.cpp
@@ -6259,6 +6259,7 @@ namespace
 #if defined(TARGET_LINUX)
         if (g_coreclr_embedded)
         {
+            // this matches exactly the names in  Interop.Libraries.cs 
             static const LPCWSTR toRedirect[] = {
                 W("libSystem.Native"),
                 W("libSystem.IO.Compression.Native"),
@@ -6266,7 +6267,7 @@ namespace
                 W("libSystem.Security.Cryptography.Native.OpenSsl")
             };
 
-            int count = sizeof(toRedirect) / sizeof(toRedirect[0]);
+            int count = lengthof(toRedirect);
             for (int i = 0; i < count; ++i)
             {
                 if (wcscmp(wszLibName, toRedirect[i]) == 0)
