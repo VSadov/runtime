e41d16334a8a127b0312b870d7f0cb6478620ff0
vsadov <8218165+VSadov@users.noreply.github.com>
2022-06-28 18:54:13 -0700

    at safeP

--- src/coreclr/nativeaot/Runtime/thread.h
+++ src/coreclr/nativeaot/Runtime/thread.h
@@ -8,6 +8,10 @@ class ThreadStore;
 class CLREventStatic;
 class Thread;
 
+#ifdef TARGET_UNIX
+#include "UnixContext.h"
+#endif
+
 // The offsets of some fields in the thread (in particular, m_pTransitionFrame) are known to the compiler and get
 // inlined into the code.  Let's make sure they don't change just because we enable/disable server GC in a particular
 // runtime build.
@@ -93,8 +97,9 @@ struct ThreadBuffer
     PTR_VOID                m_pThreadStressLog;                     // pointer to head of thread's StressLogChunks
 #ifdef FEATURE_SUSPEND_REDIRECTION
     uint8_t*                m_redirectionContextBuffer;              // storage for redirection context, allocated on demand
-    CONTEXT*                m_redirectionContext;                    // legacy context somewhere inside the context buffer
 #endif //FEATURE_SUSPEND_REDIRECTION
+    NATIVE_CONTEXT*         m_redirectionContext;                    // legacy context somewhere inside the context buffer
+
 #ifdef FEATURE_GC_STRESS
     uint32_t                m_uRand;                                // current per-thread random number
 #endif // FEATURE_GC_STRESS
