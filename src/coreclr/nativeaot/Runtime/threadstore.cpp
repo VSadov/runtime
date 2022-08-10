231862dff0a21f61d604adc7c75d36047846112b
vsadov <8218165+VSadov@users.noreply.github.com>
2022-08-09 17:21:04 -0700

    nomiss

--- src/coreclr/nativeaot/Runtime/threadstore.cpp
+++ src/coreclr/nativeaot/Runtime/threadstore.cpp
@@ -246,10 +246,13 @@ void ThreadStore::SuspendAllThreads(bool waitForGCEvent)
     // set the global trap for pinvoke leave and return
     RhpTrapThreads |= (uint32_t)TrapThreadsFlags::TrapThreads;
 
+    LARGE_INTEGER li;
+    PalQueryPerformanceCounter(&li);
+    int64_t startTicks = li.QuadPart;
+
     // Our lock-free algorithm depends on flushing write buffers of all processors running RH code.  The
     // reason for this is that we essentially implement Dekker's algorithm, which requires write ordering.
     PalFlushProcessWriteBuffers();
-    YieldProcessorNormalized();
 
     int missedHijackRetries = 0;
     int timeouts = 0;
@@ -282,7 +285,7 @@ void ThreadStore::SuspendAllThreads(bool waitForGCEvent)
                 {
                     if (pTargetThread->IsStateSet(Thread::ThreadStateFlags::TSF_FailedHijack))
                     {
-                        missedHijack = true;
+                       // missedHijack = true;
                     }
                 }
             }
@@ -331,6 +334,15 @@ void ThreadStore::SuspendAllThreads(bool waitForGCEvent)
     // preemptive mode.
     PalFlushProcessWriteBuffers();
 #endif //TARGET_ARM || TARGET_ARM64
+
+    PalQueryPerformanceFrequency(&li);
+    int64_t ticksPerSecond = li.QuadPart;
+
+    PalQueryPerformanceCounter(&li);
+    int64_t endTicks = li.QuadPart;
+    int64_t usecTotal = (endTicks - startTicks) * 1000000 / ticksPerSecond;
+
+    printf("@: %i \n", (int)usecTotal);
 }
 
 void ThreadStore::ResumeAllThreads(bool waitForGCEvent)
