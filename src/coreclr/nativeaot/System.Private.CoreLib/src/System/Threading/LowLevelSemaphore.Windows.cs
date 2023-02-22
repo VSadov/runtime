9f794cad3aa29b29e9b373d8d873d091107d2031
vsadov <8218165+VSadov@users.noreply.github.com>
2023-02-21 22:52:38 -0800

    simplified the Windows part

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Threading/LowLevelSemaphore.Windows.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Threading/LowLevelSemaphore.Windows.cs
@@ -35,22 +35,19 @@ namespace System.Threading
 
         public bool Wait(int timeoutMs)
         {
+            Debug.Assert(_nativeSemaphore != IntPtr.Zero);
             Debug.Assert(timeoutMs > 0 || timeoutMs == -1);
-            uint ret = Interop.Kernel32.WaitForSingleObject(_nativeSemaphore, (uint)timeoutMs);
 
-            switch (ret)
-            {
-                case Interop.Kernel32.WAIT_OBJECT_0:
-                    return true;
-                case Interop.Kernel32.WAIT_TIMEOUT:
-                    return false;
-                default:
-                    throw new InvalidOperationException();
-            }
+            uint ret = Interop.Kernel32.WaitForSingleObject(_nativeSemaphore, (uint)timeoutMs);
+            Debug.Assert(ret == Interop.Kernel32.WAIT_OBJECT_0 || ret == Interop.Kernel32.WAIT_TIMEOUT);
+
+            return ret == Interop.Kernel32.WAIT_OBJECT_0;
         }
 
         public void Release(int count)
         {
+            Debug.Assert(_nativeSemaphore != IntPtr.Zero);
+
             if (!Interop.Kernel32.ReleaseSemaphore(_nativeSemaphore, count, out int _))
                 throw new SemaphoreFullException();
         }
