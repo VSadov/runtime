28311756be84922131d0acd309f00f00d2542498
vsadov <8218165+VSadov@users.noreply.github.com>
2023-04-18 19:16:13 -0700

    RhpGetThreadStaticBaseForType for arm64

--- src/coreclr/nativeaot/Runtime/arm64/MiscStubs.asm
+++ src/coreclr/nativeaot/Runtime/arm64/MiscStubs.asm
@@ -5,4 +5,25 @@
 
     TEXTAREA
 
+NESTED_ENTRY RhpGetThreadStaticBaseForType, _TEXT, NoHandler
+        ;; On entry:
+        ;;   x0 - type index
+        ;; On exit:
+        ;;   x0 - the thread static base for the given type
+
+        ;; x1 = GetThread(), TRASHES x2
+        INLINE_GETTHREAD x1, x2
+
+        ;; get per-thread storage
+        ldr     x1, [x1, #OFFSETOF__Thread__m_pInlineThreadLocalStatics]
+        cbz     x1, RhpGetInlinedThreadStaticBaseSlow
+
+        ;; get the actual per-type storage
+        add     x0, x0, #2
+        ldr     x0, [x1, x0, lsl #3]  ;; x0 = *(x1 + x0 * 8)
+
+        ;; return it
+        ret
+NESTED_END RhpGetThreadStaticBaseForType, _TEXT
+
     end
