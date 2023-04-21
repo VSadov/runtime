7c0e563b4ec337c65269eeb1df521ac022200504
vsadov <8218165+VSadov@users.noreply.github.com>
2023-04-21 09:56:19 -0700

    no index for inlined storage on the runtime side

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/RuntimeImports.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/RuntimeImports.cs
@@ -566,7 +566,7 @@ namespace System.Runtime
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         [RuntimeImport(RuntimeLibrary, "RhGetInlineThreadStaticStorage")]
-        internal static extern ref object[] RhGetInlineThreadStaticStorage();
+        internal static extern ref object RhGetInlineThreadStaticStorage();
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
         [RuntimeImport(RuntimeLibrary, "RhCurrentNativeThreadId")]
