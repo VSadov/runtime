88cbb9591c342f7e753779f98ff7a0bc645c229c
vsadov <8218165+VSadov@users.noreply.github.com>
2023-12-09 12:27:20 -0800

    report GC ref/byref returns at partially interruptible callsites

--- src/coreclr/nativeaot/Runtime/windows/CoffNativeCodeManager.cpp
+++ src/coreclr/nativeaot/Runtime/windows/CoffNativeCodeManager.cpp
@@ -419,8 +419,10 @@ bool CoffNativeCodeManager::IsSafePoint(PTR_VOID pvAddress)
     if (decoder.IsInterruptible())
         return true;
 
+#if !defined(TARGET_ARM64)
     if (decoder.IsSafePoint())
         return true;
+#endif
 
     return false;
 #else
@@ -471,8 +473,11 @@ void CoffNativeCodeManager::EnumGcRefs(MethodInfo *    pMethodInfo,
             codeOffset
         );
 
-        if (decoder1.IsSafePoint())
+        if (!decoder1.IsInterruptible())
+        {
+            assert(decoder1.IsSafePoint());
             codeOffset--;
+        }
     }
 
     GcInfoDecoder decoder(
