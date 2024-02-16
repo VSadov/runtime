71371f33a2fef9fce0bd30c9f61728799e79c147
vsadov <8218165+VSadov@users.noreply.github.com>
2024-02-16 10:37:25 -0800

    IsInterruptibleSafePoint

--- src/coreclr/nativeaot/Runtime/windows/CoffNativeCodeManager.cpp
+++ src/coreclr/nativeaot/Runtime/windows/CoffNativeCodeManager.cpp
@@ -419,7 +419,7 @@ bool CoffNativeCodeManager::IsSafePoint(PTR_VOID pvAddress)
     if (decoder.IsInterruptible())
         return true;
 
-    if (decoder.IsSafePoint())
+    if (decoder.IsInterruptibleSafePoint())
         return true;
 
     return false;
@@ -479,6 +479,8 @@ void CoffNativeCodeManager::EnumGcRefs(MethodInfo *    pMethodInfo,
                 GcInfoDecoderFlags(DECODE_GC_LIFETIMES | DECODE_SECURITY_OBJECT | DECODE_VARARG),
                 codeOffset - 1
             );
+
+            assert(decoder.IsInterruptibleSafePoint());
         }
     }
 
