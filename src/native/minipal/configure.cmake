59305baba2af5da7e90e37b6a12c23b8017568e2
vsadov <8218165+VSadov@users.noreply.github.com>
2024-06-06 15:51:01 -0700

    f

--- src/native/minipal/configure.cmake
+++ src/native/minipal/configure.cmake
@@ -13,18 +13,20 @@ check_symbol_exists(
     time.h
     HAVE_CLOCK_GETTIME_NSEC_NP)
 
-check_cxx_source_runs("
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
-
-int main()
-{
-  int ret;
-  struct timespec ts;
-  ret = clock_gettime(CLOCK_MONOTONIC, &ts);
-
-  exit(ret);
-}" HAVE_CLOCK_MONOTONIC)
+check_c_source_runs(
+    "
+    #include <stdlib.h>
+    #include <time.h>
+    #include <sys/time.h>
+    int main(void)
+    {
+        int ret;
+        struct timespec ts;
+        ret = clock_gettime(CLOCK_MONOTONIC, &ts);
+        exit(ret);
+        return 0;
+    }
+    "
+    HAVE_CLOCK_MONOTONIC)
 
 configure_file(${CMAKE_CURRENT_LIST_DIR}/minipalconfig.h.in ${CMAKE_CURRENT_BINARY_DIR}/minipalconfig.h)
