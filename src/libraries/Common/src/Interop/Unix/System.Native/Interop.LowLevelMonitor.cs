e280056103c126b66fbfe8afcb02f83abb168a4c
vsadov <8218165+VSadov@users.noreply.github.com>
2026-01-24 21:45:21 -0800

    f

--- src/libraries/Common/src/Interop/Unix/System.Native/Interop.LowLevelMonitor.cs
+++ src/libraries/Common/src/Interop/Unix/System.Native/Interop.LowLevelMonitor.cs
@@ -34,14 +34,14 @@ internal static partial class Interop
 
 
         [LibraryImport(Libraries.SystemNative, EntryPoint = "SystemNative_LowLevelFutex_WaitOnAddress")]
-        internal static void LowLevelFutex_WaitOnAddress(int32_t* address, int32_t comparand);
+        internal static void LowLevelFutex_WaitOnAddress(IntPtr address, int32_t comparand);
 
         [LibraryImport(Libraries.SystemNative, EntryPoint = "SystemNative_LowLevelFutex_WaitOnAddressTimeout")]
         [return: MarshalAs(UnmanagedType.Bool)]
-        internal static bool LowLevelFutex_WaitOnAddressTimeout(int32_t* address, int32_t comparand, int32_t timeoutMilliseconds);
+        internal static bool LowLevelFutex_WaitOnAddressTimeout(IntPtr address, int32_t comparand, int32_t timeoutMilliseconds);
 
         [SuppressGCTransition]
         [LibraryImport(Libraries.SystemNative, EntryPoint = "SystemNative_LowLevelFutex_WakeByAddressSingle")]
-        internal static void LowLevelFutex_WakeByAddressSingle(int32_t* address);
+        internal static void LowLevelFutex_WakeByAddressSingle(IntPtr address);
     }
 }
