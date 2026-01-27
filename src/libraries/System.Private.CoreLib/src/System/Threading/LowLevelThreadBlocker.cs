a93f63a7cce7c788b1fa1b1e3f44aa9c1d1f720c
vsadov <8218165+VSadov@users.noreply.github.com>
2026-01-26 16:52:01 -0800

    define

--- src/libraries/System.Private.CoreLib/src/System/Threading/LowLevelThreadBlocker.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/LowLevelThreadBlocker.cs
@@ -1,7 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if !TARGET_LINUX && !TARGET_WINDOWS
+#if TARGET_LINUX || TARGET_WINDOWS
+// use OS-provided compare-and-block API.
+#else
+// fallback to monitor
 #define USE_MONITOR
 #endif
 
